5c5f8810201e5b49e2607b85122b2a67b6836181
MustafaOmran <42530569+MustafaOmran@users.noreply.github.com>
2018-11-29 23:56:46 -0500

    updated prop

--- linearProb.java
+++ linearProb.java
@@ -5,7 +5,6 @@ public class linearProb extends hashtable{
 		super(n);
 		size = 0; 
 	}
-	///asdadasa
 
 	public boolean isEmpty() {
 		return size == 0; 
@@ -29,6 +28,9 @@ public class linearProb extends hashtable{
 		String temp1 = null; 
 		int i =hasCode(k, table.length);
 		for (i =hasCode(k, table.length); table[i] != null ; i ++) {
+			if (table[i].getnodeKey() == k) {
+				temp1 = table[i].findseparate(temp, "remove");
+				}
 			if ( (i+1) == table.length) {
 				i = -1;
 			}
@@ -43,9 +45,18 @@ public class linearProb extends hashtable{
 	@Override
 	public String remove(int k) {
 		
-		map temp = new map(); 
+		map temp = new map (k, value); 
 		String temp1 = null; 
-		temp1 = table[temp.hasCode(k, table.length)].findseparate(temp,"put");
+		int i =hasCode(k, table.length);
+		for (i =hasCode(k, table.length); table[i] != null ; i ++) {
+			if (table[i].getnodeKey() == k) {
+				temp1 = table[i].findseparate(temp, "remove");
+				}
+			if ( (i+1) == table.length) {
+				i = -1;
+			}
+		}
+		System.out.println("Value retrived =  "+temp1 +", the size of table is " + table.length +",  number of elements = " + size);
 		size--; 
 		return temp1;
 	}
